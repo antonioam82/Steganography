faf8ac7cdceb10a6abe8b671367ce9e029d45ae8
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-03-05 11:15:43 +0100

    Update steganopy_GUI0.py

--- steganopy_GUI0.py
+++ steganopy_GUI0.py
@@ -3,9 +3,8 @@ from tkinter import ttk
 from tkinter import messagebox, filedialog
 import tkinter.scrolledtext as sct
 import threading
-import pyperclip
-import re
 import time
+import re
 import cv2
 import os
 import numpy as np
@@ -29,28 +28,24 @@ class app():
         self.entryDir.place(x=0,y=0)
         self.textEntry = sct.ScrolledText(self.window,width=70,height=15)
         self.textEntry.place(x=5,y=28)
-        self.btnCopy = Button(self.window,text="COPY TEXT",bg=self.backgr,command=self.init_copy)
-        self.btnCopy.place(x=5,y=277)
-        self.btnClear = Button(self.window,text="CLEAR TEXT",bg=self.backgr,command=self.clear)
-        self.btnClear.place(x=80,y=277)
+        self.btnClear = Button(self.window,text="CLEAR TEXT",width=20,bg=self.backgr,command=self.clear)
+        self.btnClear.place(x=5,y=277)
         self.rdbEncode = Radiobutton(self.window,text="Encode",variable=self.mode,value="EN",command=self.set_mode)
         self.rdbEncode.place(x=420,y=277)
         self.rdbDecode = Radiobutton(self.window,text="Decode",variable=self.mode,value="DE",command=self.set_mode)
         self.rdbDecode.place(x=506,y=277)
         self.btnSearch = Button(self.window,text="SEARCH",width=20,bg=self.backgr,command=self.open_file)
         self.btnSearch.place(x=5,y=315)
-        self.entImage = Entry(self.window,width=38,font=('arial',14),textvariable=self.imaname)
-        self.entImage.place(x=160,y=315)
-        self.btnStart = Button(self.window,text="START ENCODING",width=58,bg=self.backgr,command=self.init_task)
-        self.btnStart.place(x=167,y=353)
+        self.entImage = Entry(self.window,width=37,font=('arial',14),textvariable=self.imaname)
+        self.entImage.place(x=167,y=315)
+        self.btnStart = Button(self.window,text="START ENCODING",width=81,bg=self.backgr,command=self.init_task)
+        self.btnStart.place(x=5,y=353)
         self.bylab = Label(self.window,text="BYTES AVAILABLE:")
         self.bylab.place(x=167,y=280)
         self.byEnt = Entry(self.window,textvariable=self.nbytes,width=17)
         self.byEnt.place(x=275,y=281)
         self.invLabel = Label(self.window,text="",fg="blue",width=83)
         self.invLabel.place(x=3,y=380)
-        self.btnSave = Button(self.window,text="SAVE DATA",width=20,bg=self.backgr,command=self.save_data)
-        self.btnSave.place(x=5,y=353)
         
 
         self.show_dir()
@@ -59,21 +54,6 @@ class app():
 
     def show_dir(self):
         self.current_dir.set(os.getcwd())
-
-    def save_data(self):
-        if len(self.textEntry.get('1.0',END)) > 1:
-            document = filedialog.asksaveasfilename(initialdir="/",
-                       title="Save",defaultextension='.txt')
-            if document != "":
-                file = open(document,"w",encoding="utf-8")
-                line = ""
-                content = self.textEntry.get('1.0',END)
-                for l in content:
-                    line = line+l
-                file.write(line)
-                file.close()
-                messagebox.showinfo("SAVED","Saved document: {}".format(document))
-            
 
     def set_mode(self):
         self.btnStart.configure(text="START {}CODING".format(self.mode.get()))
@@ -106,55 +86,33 @@ class app():
         else:
             raise TypeError("Type not supported.")
 
-    def copy_text(self):
-        self.textEntry.delete('1.0',END)
-        self.ultima_copia = pyperclip.paste().strip()
-        while True:
-            time.sleep(0.1)
-            self.copia = pyperclip.paste().strip()
-            if self.copia != self.ultima_copia:
-                self.textEntry.insert(END,self.copia)
-                self.ultima_copia = self.copia 
-                break
-
-    def init_copy(self):
-        messagebox.showinfo("IMPORT TEXT","""Select the text, right-click and
-select 'copy' to import it.""")
-        t1 = threading.Thread(target=self.copy_text)
-        t1.start()
-
     def encode(self):
-        #secret_data = re.sub(r"[^a-zA-Z0-9-\s-.-(-)]","",self.textEntry.get('1.0',END))#self.textEntry.get('1.0',END)
-        secret_data = self.textEntry.get('1.0',END)
+        #secret_data = self.textEntry.get('1.0',END)
+        secret_data = re.sub("\[\d+\]","",self.textEntry.get('1.0',END))
         if len(secret_data) <= self.n_bytes:
-            new_file = filedialog.asksaveasfilename(initialdir="/",filetypes=[('png files','*.PNG'),
-                                     ('tiff files','*.TIFF')],title="Save",defaultextension='.png')
-            ima_name = (new_file).split("/")[-1]
-            #print(ima_name)
-            if new_file != "":
-                secret_data += "====="
-                data_index = 0
-                binary_secret_data = self.to_bin(secret_data)
-                data_len = len(binary_secret_data)
-                for row in self.image:
-                    for pixel in row:
-                        r, g, b = self.to_bin(pixel)
-                        if data_index < data_len:
-                            pixel[0] = int(r[:-1] + binary_secret_data[data_index], 2)
-                            data_index += 1
-                        if data_index < data_len:
-                            pixel[1] = int(g[:-1] + binary_secret_data[data_index], 2)
-                            data_index += 1
-                        if data_index < data_len:
-                            pixel[2] = int(b[:-1] + binary_secret_data[data_index], 2)
-                            data_index += 1
-                        if data_index >= data_len:
-                            break
-            
-                cv2.imwrite(new_file,self.image)
-                messagebox.showinfo("TASK COMPLETED","Created image: {}".format(ima_name))
+            secret_data += "====="
+            data_index = 0
+            binary_secret_data = self.to_bin(secret_data)
+            data_len = len(binary_secret_data)
+            for row in self.image:
+                for pixel in row:
+                    r, g, b = self.to_bin(pixel)
+                    if data_index < data_len:
+                        pixel[0] = int(r[:-1] + binary_secret_data[data_index], 2)
+                        data_index += 1
+                    if data_index < data_len:
+                        pixel[1] = int(g[:-1] + binary_secret_data[data_index], 2)
+                        data_index += 1
+                    if data_index < data_len:
+                        pixel[2] = int(b[:-1] + binary_secret_data[data_index], 2)
+                        data_index += 1
+                    if data_index >= data_len:
+                        break
+            ima_name = "encoded_"+self.file_name
+            cv2.imwrite(ima_name,self.image)
+            messagebox.showinfo("TASK COMPLETED","Created image: {}".format(ima_name))
         else:
-            messagebox.showwarning("NO ESPACE","Insufficient bytes, need bigger image or less data.")
+            messagebox.showwarning("ERROR","Insufficient bytes, need bigger image or less data.")
         self.invLabel.configure(text="")
         self.running = False
                     
